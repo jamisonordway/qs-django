b6b29c053fc90602de80c093a6403b9d63b03f68
JamisonOrdway <jamisonordway@gmail.com>
2018-08-27 15:25:32 -0600

    adds Procfile

--- qs_backend/qs_backend/settings.py
+++ qs_backend/qs_backend/settings.py
@@ -1,5 +1,5 @@
+import django_heroku
 import os
-import dj_database_url
 
 ROOT_PATH = os.path.dirname(__file__)
 STATICFILES_DIRS = [os.path.join(ROOT_PATH, 'static')]
@@ -133,3 +133,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+django_heroku.settings(locals())
